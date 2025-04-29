9a7a97732f6cba93860fbdce37bc0d23d24a19b5
Laura <lauratan2000@outlook.com>
2025-04-29 16:53:43 -0700

    lab3

---
+++ lab3/sorting_hat_ESP32/sorting_hat_esp_button_64_width/sorting_hat_model.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <cstdarg>
+namespace Eloquent {
+    namespace ML {
+        namespace Port {
+            class DecisionTree {
+                public:
+                    /**
+                    * Predict class for features vector
+                    */
+                    int predict(float *x) {
+                        if (x[0] <= 3.5) {
+                            if (x[0] <= 2.5) {
+                                if (x[5] <= 1.5) {
+                                    if (x[8] <= 1.5) {
+                                        return 0;
+                                    }
+
+                                    else {
+                                        if (x[9] <= 1.5) {
+                                            return 0;
+                                        }
+
+                                        else {
+                                            return 1;
+                                        }
+                                    }
+                                }
+
+                                else {
+                                    if (x[0] <= 1.5) {
+                                        if (x[2] <= 1.5) {
+                                            return 0;
+                                        }
+
+                                        else {
+                                            return 2;
+                                        }
+                                    }
+
+                                    else {
+                                        if (x[8] <= 3.5) {
+                                            return 1;
+                                        }
+
+                                        else {
+                                            return 3;
+                                        }
+                                    }
+                                }
+                            }
+
+                            else {
+                                if (x[1] <= 3.5) {
+                                    if (x[2] <= 2.5) {
+                                        if (x[1] <= 2.5) {
+                                            return 0;
+                                        }
+
+                                        else {
+                                            return 2;
+                                        }
+                                    }
+
+                                    else {
+                                        if (x[2] <= 3.5) {
+                                            return 2;
+                                        }
+
+                                        else {
+                                            return 2;
+                                        }
+                                    }
+                                }
+
+                                else {
+                                    return 3;
+                                }
+                            }
+                        }
+
+                        else {
+                            if (x[9] <= 3.5) {
+                                if (x[8] <= 3.5) {
+                                    if (x[9] <= 2.5) {
+                                        if (x[1] <= 1.5) {
+                                            return 0;
+                                        }
+
+                                        else {
+                                            return 1;
+                                        }
+                                    }
+
+                                    else {
+                                        return 2;
+                                    }
+                                }
+
+                                else {
+                                    return 3;
+                                }
+                            }
+
+                            else {
+                                return 3;
+                            }
+                        }
+                    }
+
+                protected:
+                };
+            }
+        }
+    }
